98c2586170bd910dd96bc7bc340d36ca7ef25d2d
uzaaft <muaf@nmbu.no>
2021-10-12 14:06:40 +0200

    Fixed .service file

--- install_service.sh
+++ install_service.sh
@@ -1,4 +1,4 @@
 echo "Asking for password to move bee_ctrl.service to /lib/systemd/system"
 sudo cp bee_ctrl.service /lib/systemd/system/
-systemctl enable ngrok.service
-systemctl start ngrok.service
+systemctl enable bee_ctrl.service
+systemctl start bee_ctrl.service
